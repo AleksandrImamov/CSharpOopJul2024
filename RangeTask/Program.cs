376f257c18d0d5ca85cac0d96903b0bb4bdc6a0f
AleksandrImamov <leksandr1989@gmail.com>
2024-07-11 23:39:12 +0700

    Range Task

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!123");
+            Console.WriteLine("Введите начальное число диапазона");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное число диапазона");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            Range range = new Range(from, to);
+                        
+            Console.WriteLine("Длина диапазона равна : " + range.GetRangeLength());
+
+            Console.WriteLine("Введите любое число");
+            double inputNumber = Convert.ToDouble(Console.ReadLine());
+
+            if (range.IsInside(inputNumber))
+            {
+                Console.WriteLine("Число " + inputNumber + " принадлежит диапазону");
+            }
+            else
+            {
+                Console.WriteLine("Число " + inputNumber + " не принадлежит диапазону");
+            }
         }
     }
 }
---
+++ RangeTask/Range.cs
@@ -0,0 +1,49 @@
+﻿namespace RangeTask
+{
+    public class Range
+    {
+        private double _from;
+        private double _to;
+
+        public Range(double from, double to)
+        {
+            _from = from;
+            _to = to;
+        }
+
+        public double GetFrom()
+        {
+            return _from;
+        }
+
+        public void SetFrom(double from)
+        {
+            _from = from;
+        }
+
+        public double GetTo()
+        {
+            return _to;
+        }
+
+        public void SetTo(double to)
+        {
+            _to = to;
+        }
+
+        public double GetRangeLength()
+        {
+            return _to - _from;
+        }
+
+        public bool IsInside(double inputNumber)
+        {
+            if (inputNumber >= _from && inputNumber <= _to)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

495e08a667a6af4665f0daf53290f34504214b98
AleksandrImamov <leksandr1989@gmail.com>
2024-07-15 13:26:59 +0700

    Range task

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -11,7 +11,7 @@
             double to = Convert.ToDouble(Console.ReadLine());
 
             Range range = new Range(from, to);
-                        
+
             Console.WriteLine("Длина диапазона равна : " + range.GetRangeLength());
 
             Console.WriteLine("Введите любое число");
@@ -24,6 +24,54 @@
             else
             {
                 Console.WriteLine("Число " + inputNumber + " не принадлежит диапазону");
+            }
+
+            Console.WriteLine("Введите начальное число диапазона 1");
+            double from1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное число диапазона 1");
+            double to1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите начальное число диапазона 2");
+            double from2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное число диапазона 2");
+            double to2 = Convert.ToDouble(Console.ReadLine());
+
+            Range range1 = new Range(from1, to1, from2, to2);
+
+            Range rangeIntersection = range1.GetRangeIntersection();
+
+            if (rangeIntersection is not null)
+            {
+                Console.WriteLine("Интервал-пересечение двух интервалов равен " + (rangeIntersection.To - rangeIntersection.From));
+            }
+            else
+            {
+                Console.WriteLine("Нет интервала-пересечения");
+            }
+
+            Range range2 = new Range(from1, to1, from2, to2);
+
+            Range[] rangeUnion = range2.GetRangeUnion();
+
+            foreach (Range e in rangeUnion)
+            {
+                Console.WriteLine("Объединение двух интервалов равно" + (e.To - e.From));
+            }
+
+            Range range3 = new Range(from1, to1, from2, to2);
+
+            Range[] rangeDifference = range3.GetRangeDifference();
+
+            foreach (Range e in rangeDifference)
+            {
+                if (rangeDifference is null)
+                {
+                    Console.WriteLine("Интервалы не пересекаются");
+                }
+
+                Console.WriteLine("Разность двух интервалов равна" + (e.To - e.From));
             }
         }
     }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -2,48 +2,80 @@
 {
     public class Range
     {
-        private double _from;
-        private double _to;
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public double From2 { get; set; }
+
+        public double To2 { get; set; }
+
 
         public Range(double from, double to)
         {
-            _from = from;
-            _to = to;
+            From = from;
+            To = to;
         }
 
-        public double GetFrom()
+        public Range(double from1, double to1, double from2, double to2)
         {
-            return _from;
-        }
-
-        public void SetFrom(double from)
-        {
-            _from = from;
-        }
-
-        public double GetTo()
-        {
-            return _to;
-        }
-
-        public void SetTo(double to)
-        {
-            _to = to;
+            From = from1;
+            To = to1;
+            From2 = from2;
+            To2 = to2;
         }
 
         public double GetRangeLength()
         {
-            return _to - _from;
+            return To - From;
         }
 
         public bool IsInside(double inputNumber)
         {
-            if (inputNumber >= _from && inputNumber <= _to)
+            if (inputNumber >= From && inputNumber <= To)
             {
                 return true;
             }
 
             return false;
+        }
+
+        public Range GetRangeIntersection()
+        {
+            if (To > From2)
+            {
+                return new Range(From2, To);
+            }
+
+            return null;
+        }
+
+        public Range[] GetRangeUnion()
+        {
+            if (To >= From2)
+            {
+                return new Range[] { new Range(From, To2) };
+            }
+
+            return new Range[] { new Range(From, To), new Range(From2, To2) };
+        }
+
+        public Range[] GetRangeDifference()
+        {
+            if (From == From2 && To == To2)
+            {
+                return new Range[] { new Range(0, 0) };
+            }
+            else if (From2 < To)
+            {
+                return new Range[] { new Range(From, From2) };
+            }
+            else if (From2 > From && To2 < To)
+            {
+                return new Range[] { new Range(From, From2), new Range(To2, To) };
+            }
+
+            return null;
         }
     }
 }
